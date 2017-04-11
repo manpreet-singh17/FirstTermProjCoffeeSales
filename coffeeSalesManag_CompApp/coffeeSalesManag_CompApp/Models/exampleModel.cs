6f4e4db463c222f131f4176d994656239acdc600
manpreet singh <mpsingh0001@gmail.com>
2017-04-12 11:53:31 +1200

    An example model class is added to models folder.

---
+++ coffeeSalesManag_CompApp/coffeeSalesManag_CompApp/Models/exampleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coffeeSalesManag_CompApp.Models
+{
+    public class exampleModel
+    {
+        public int ExampleProperty { get; set; }
+
+    }
+}
